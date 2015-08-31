b64110b4abb68f1693c7f9282f36d7a2cd8e93d8
Pieter Colpaert <pieter.colpaert@okfn.org>
2015-08-31 17:30:34 +0200

    Added first test

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var fs = require('fs'),
+    should = require('should'),
+    async = require('async');
+
+describe('Creating lists', function () {
+  //Create a stream of arrivals
+
+  //Create a stream of departures
+
+  //Now, they should be able to emit connections
+  it('should be emitting connections', function () {
+
+  });
+
+})
