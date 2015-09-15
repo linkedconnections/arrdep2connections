0c48dfb16d20e38733111a1f45d2d01636225f30
brechtvdv <br5cht@hotmail.com>
2015-09-15 18:45:57 +0200

    Update test with paramater options

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     should = require('should'),
     jsonldstream = require('jsonld-stream'),
+    param = require('param'),
     ArrDep2Connections = require('../lib/arrdep2connections.js');
 
 describe('Creating lists', function () {
@@ -10,8 +11,14 @@ describe('Creating lists', function () {
   var departuresStream = fs.createReadStream('./test/data/departures.jsonldstream', {encoding: 'utf8'}).pipe(new jsonldstream.Deserializer());
   //Now, they should be able to emit connections when combined in the code
   it('should be emitting connections', function (done) {
-   
-    var transformer = new ArrDep2Connections(arrivalsStream);
+    // Read standard configuration parameters
+    var options = {};
+    options['mongoDb'] = param('mongodb');
+    options['mongoDbConfig'] = param('mongoDbConfig');
+    options['inbound'] = param('inbound');
+    options['outbound'] = param('outbound');
+
+    var transformer = new ArrDep2Connections(arrivalsStream, options);
     var stream = departuresStream.pipe(transformer);
     var count = 0;
     stream.on("data", function (connection) {
